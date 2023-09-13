f9ba9cc2f4c26e19702c3e7210b2b2cdd279f948
Vishal Kumar <vishal.ak5262@gmail.com>
2023-09-13 21:01:17 +0530

    Add files via upload

---
+++ config/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: "mongodb://vishaldeoghar:tkQHQHn3tqV5@ac-kbpsxqf-shard-00-00.lpb5inx.mongodb.net:27017,ac-kbpsxqf-shard-00-01.lpb5inx.mongodb.net:27017,ac-kbpsxqf-shard-00-02.lpb5inx.mongodb.net:27017/?authSource=admin&replicaSet=atlas-14cwvh-shard-0&ssl=true"
+}
